3161316bac44d72428642f7dccbcc257de12dea7
MauroLucas <m_l_pereyra@hotmail.com>
2017-09-11 17:12:07 -0300

    Adjunto carpeta de trabajos practicos de objetos 1 hasta la fecha

---
+++ eclipse-workspace/Objetos 1/Listas/modelo/Pelicula.java
@@ -0,0 +1,39 @@
+package modelo;
+
+public class Pelicula {
+	
+	private int idPelicula;
+	private String pelicula;
+	
+	public Pelicula() {}
+	public Pelicula(int idPelicula, String pelicula) {
+		super();
+		this.idPelicula = idPelicula;
+		this.pelicula = pelicula;
+	}
+	
+	@Override
+	public String toString() {
+		return "Pelicula [idPelicula=" + idPelicula + ", pelicula=" + pelicula + "]";
+	}
+	public int getIdPelicula() {
+		return idPelicula;
+	}
+	public void setIdPelicula(int idPelicula) {
+		this.idPelicula = idPelicula;
+	}
+	public String getPelicula() {
+		return pelicula;
+	}
+	public void setPelicula(String pelicula) {
+		this.pelicula = pelicula;
+	}
+	public boolean equals(Pelicula pelicula) {
+		boolean resultado=false;
+		if(idPelicula==pelicula.getIdPelicula())resultado=true;
+		return resultado;
+	}
+	
+	
+
+}
